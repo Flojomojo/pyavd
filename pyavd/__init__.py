b9fbb24ca66b3be6062724980547cd531525f01e
Flojomojo <huemmer@gmx.de>
2024-02-05 00:39:46 +0100

    added device, avd and target to export

--- pyavd/__init__.py
+++ pyavd/__init__.py
@@ -3,5 +3,11 @@ from .pyavd import get_avds
 from .pyavd import get_devices
 from .pyavd import create_avd
 from .pyavd import get_avd_by_name
+from .pyavd import Device
+from .pyavd import AVD
+from .pyavd import Target
+
+
+
 
 __version__ = "0.0.1"
